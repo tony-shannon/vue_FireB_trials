c75a89afbad8139c741158858ed46f9aa71ed918
Tony Shannon <tony.shannon@ripple.foundation>
2020-02-03 17:55:42 +0000

    added firebase connection & code

--- notebook/src/App.vue
+++ notebook/src/App.vue
@@ -8,6 +8,17 @@
     <script>
     import Notebook from './components/Notebook'
     import Page from './components/Page'
+    import Firebase from 'firebase'
+
+    var database = Firebase.initializeApp({
+      apiKey: "AIzaSyC2DR-dhFDL6z_oKZlXGxW9jhQYnxfWsqU",
+      authDomain: "notebook-1a.firebaseapp.com",
+      databaseURL: "https://notebook-1a.firebaseio.com",
+      projectId: "notebook-1a",
+      storageBucket: "notebook-1a.appspot.com",
+      messagingSenderId: "292619954408",
+      appId: "1:292619954408:web:6fa6b2abd180dd60ed015d"
+    }).database().ref();
 
     export default {
       name: 'app',
@@ -19,6 +30,17 @@
         pages: [],
         index: 0
       }),
+      mounted() {
+      database.once('value', (pages) => {
+        pages.forEach((page) => {
+          this.pages.push({
+            ref: page.ref,
+            title: page.child('title').val(),
+            content: page.child('content').val()
+          })
+        })
+      })
+      },
       methods: {
         newPage () {
           this.pages.push({
@@ -32,8 +54,25 @@
         },
         savePage () {
            // nothing as of yet
+          var page = this.pages[this.index]      // added for firebase
+          if (page.ref) {
+                this.updateExistingPage(page)
+              } else {
+                this.insertNewPage(page)        // added for firebase
+              }
         },
+        updateExistingPage (page) {           //added for firebase
+              page.ref.update({
+                title: page.title,
+                content: page.content
+              })
+            },
+        insertNewPage (page) {                //added for firebase
+            page.ref = database.push(page)
+          },
         deletePage () {
+          var ref = this.pages[this.index].ref  // for firebase
+          ref && ref.remove()                   // for firebase
           this.pages.splice(this.index, 1)
           this.index = Math.max(this.index - 1, 0)
         }
